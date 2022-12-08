137b900d2fbcb072fabb722af04f879200520f2f
ritik72199 <119751590+ritik72199@users.noreply.github.com>
2022-12-08 09:29:38 +0530

    database

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const { createPool } = require("mysql");
 
 const pool = createPool({
   port: 3306,
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
+  host: "148.66.136.10",
+  user: "TEST_ROGER",
+  password: "roger1234",
   database: "ROGER_DB",
   connectionLimit: 150,
 });
